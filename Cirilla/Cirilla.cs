b2cb5900995bfa496a61be5b97c0bc5e417764c9
mrousavy <marcrousavy@hotmail.com>
2017-06-10 17:34:10 +0200

    Added basic login and Console Intro

---
+++ Cirilla/Cirilla.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Cirilla {
+    public class Cirilla : IDisposable {
+        #region Privates
+        private DiscordSocketClient _client;
+        #endregion
+
+        #region Publics
+        public bool IsDisposed { get; set; }
+        #endregion
+
+        public Cirilla() {
+            DiscordSocketConfig config = new DiscordSocketConfig {
+                LogLevel = LogSeverity.Info
+            };
+            _client = new DiscordSocketClient(config);
+            _client.Log += Log;
+
+            Login().GetAwaiter().GetResult();
+        }
+
+        public void Dispose() {
+            _client.LogoutAsync().GetAwaiter().GetResult();
+            _client.Dispose();
+        }
+
+        public async Task Login() {
+            await _client.LoginAsync(TokenType.Bot, "bot token");
+            await _client.StartAsync();
+        }
+
+        private Task Log(LogMessage message) {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- Cirilla/Program.cs
+++ Cirilla/Program.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Threading;
 
 namespace Cirilla {
     public class Program {
+        internal static Cirilla Cirilla;
+
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Thread.Sleep(3000);
+
+            ConsoleHelper.Set();
+            Intro();
+
+            Cirilla = new Cirilla();
+
+            Thread.Sleep(-1);
+        }
+
+
+        public static void Intro() {
+            Console.Title = "Cirilla Discord Bot";
+
+            string introText = "~Cirilla~";
+            int left = (Console.WindowWidth / 2) - (introText.Length / 2);
+            int top = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(left, top);
+            ConsoleColor color = Console.ForegroundColor = ConsoleColor.Cyan;
+
+            foreach (char ch in introText.ToCharArray()) {
+                Console.Write(ch);
+                Thread.Sleep(90);
+            }
+
+
+            Console.ForegroundColor = color;
+
+            Thread.Sleep(2500);
+
+            Console.Clear();
         }
     }
 }

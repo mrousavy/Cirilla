3bfe9ff715b3b76c1e1982201c048f69db6740a4
mrousavy <marcrousavy@hotmail.com>
2017-06-10 16:55:37 +0200

    Added Project files

---
+++ Cirilla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cirilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

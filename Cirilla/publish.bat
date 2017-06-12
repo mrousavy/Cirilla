e42ef77bbe3e12e054c403bef9db86d4fb8f2db7
mrousavy <marcrousavy@hotmail.com>
2017-06-12 20:47:31 +0200

    Updated Scripts

--- Cirilla/publish.bat
+++ Cirilla/publish.bat
@@ -1,3 +1,3 @@
 SET mypath=%~dp0
-dotnet build -f netcoreapp2.0 -c Release
-dotnet publish -f netcoreapp2.0 -c Release
+dotnet build -f netcoreapp1.1 -c Release
+dotnet publish -f netcoreapp1.1 -c Release

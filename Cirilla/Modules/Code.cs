eb3dd72a7ac6da97ce8790665c348a144ef0ae32
mrousavy <marcrousavy@hotmail.com>
2017-06-14 18:20:42 +0200

    Added Script Exec template

---
+++ Cirilla/Modules/Code.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Cirilla.Modules {
+    public class Code : ModuleBase {
+        [Command("exec"), Summary("Execute or run C# Code/Scripts")]
+        public async Task Execute() {
+            //TODO:
+            await ReplyAsync("Sorry, this is a work in progress!");
+        }
+    }
+}

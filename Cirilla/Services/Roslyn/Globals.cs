d6cc08506ff9b85feab3957eef2b9ea593507dc0
mrousavy <mrousavy>
2017-06-19 09:07:34 +0100

    Changed Roslyn Globals Type

--- Cirilla/Services/Roslyn/Globals.cs
+++ Cirilla/Services/Roslyn/Globals.cs
@@ -1,11 +1,12 @@
 ﻿using Discord;
 using System;
 using System.IO;
+using Discord.WebSocket;
 
 namespace Cirilla.Services.Roslyn {
     public class Globals {
         public TextWriter Console { get; set; }
         public Random Random { get; set; }
-        public IDiscordClient Client { get; set; }
+        public DiscordSocketClient Client { get; set; }
     }
 }

4deaf75aceee9000d3c3877a59fa013321e7d960
mrousavy <marcrousavy@hotmail.com>
2017-06-24 12:04:37 +0200

    Randoms syntax

--- Cirilla/Modules/Randoms.cs
+++ Cirilla/Modules/Randoms.cs
@@ -6,14 +6,14 @@ namespace Cirilla.Modules {
     public class Randoms : ModuleBase {
         [Command("flip"), Summary("Flip a Coin!")]
         public async Task Flip() {
-            int result = new Random().Next(0, 2);
+            int result = new Random().Next(2);
             await ReplyAsync("Coin flip: " + (result == 0 ? "Heads!" : "Tails!"));
         }
 
 
         [Command("random"), Summary("Generate a Random number")]
         public async Task Random([Summary("Maximum Value")] int maximum) {
-            int result = new Random().Next(0, maximum) + 1;
+            int result = new Random().Next(maximum) + 1;
             await ReplyAsync(result.ToString());
         }
     }

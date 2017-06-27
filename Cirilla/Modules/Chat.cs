311d3ad0b5088574ccf7b6d8c23859e284692820
mrousavy <marcrousavy@hotmail.com>
2017-06-27 16:53:45 +0200

    Added optional poke messages

--- Cirilla/Modules/Chat.cs
+++ Cirilla/Modules/Chat.cs
@@ -3,7 +3,7 @@ using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace Cirilla.Modules {
-    public class Greetings : ModuleBase {
+    public class Chat : ModuleBase {
         [Command("hi"), Summary("Greets the User")]
         public async Task Hi() {
             await ReplyAsync("Hi! :wave: How are you doing?"); //ask user how he is doing (we don't care tho)
@@ -17,8 +17,29 @@ namespace Cirilla.Modules {
         [Command("poke"), Summary("Pokes a user ¯\\_(ツ)_/¯")]
         public async Task Poke([Summary("The User to poke")] IUser user) {
             try {
+                if (user.IsBot) {
+                    await ReplyAsync("You can't poke bots, silly! :rolling_eyes:");
+                    return;
+                }
+
                 IDMChannel dm = await user.CreateDMChannelAsync();
                 await dm.SendMessageAsync($"Hey! **{Context.User.Mention}** from **{Context.Guild.Name}** poked you!", true);
+                await ReplyAsync($"Poked {Helper.GetName(user)}!");
+            } catch {
+                await ReplyAsync($"I couldn't poke {Helper.GetName(user)}, sorry!");
+            }
+        }
+
+        [Command("poke"), Summary("Pokes a user ¯\\_(ツ)_/¯")]
+        public async Task Poke([Summary("The User to poke")] IUser user, [Summary("The poke message")][Remainder] string text) {
+            try {
+                if (user.IsBot) {
+                    await ReplyAsync("You can't poke bots, silly! :rolling_eyes:");
+                    return;
+                }
+
+                IDMChannel dm = await user.CreateDMChannelAsync();
+                await dm.SendMessageAsync($"Hey! **{Context.User.Mention}** from **{Context.Guild.Name}** poked you, saying: _\"{text}\"_", true);
                 await ReplyAsync($"Poked {Helper.GetName(user)}!");
             } catch {
                 await ReplyAsync($"I couldn't poke {Helper.GetName(user)}, sorry!");

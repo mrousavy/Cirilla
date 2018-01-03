66dc96cd50fd1ded097bc7bf335baeb8292d5398
mrousavy <mrousavy@donotreply.github.com>
2018-01-03 17:44:20 +0100

    Implement Pokedex by ID Module

---
+++ Cirilla/Modules/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cirilla.Services.Pokemon;
+using Discord.Commands;
+
+namespace Cirilla.Modules {
+    public class Pokemon : ModuleBase {
+        [Command("pokedex")]
+        [Summary("Search a Pokémon in the national Pokédex by it's ID")]
+        public async Task FindPokemon([Summary("The national Pokédex ID of the Pokémon to lookup")] int id) {
+            try {
+                var pokemon = await Pokedex.GetPokemonById(id);
+                // TODO: Build Pokemon Embed
+            } catch (Exception ex) {
+                await ReplyAsync(ex.Message);
+            }
+        }
+    }
+}

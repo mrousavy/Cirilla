d552e938ac1772923c78d69847482c1c47872f53
mrousavy <mrousavy@donotreply.github.com>
2018-01-03 17:44:08 +0100

    Implement Pokedex API builder

---
+++ Cirilla/Services/Pokemon/Pokedex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cirilla.Services.Pokemon {
+    public static class Pokedex {
+        public static async Task<Pokemon> GetPokemonById(int id) {
+            using (var client = new HttpClient()) {
+                var response = await client.PostAsync($"http://pokeapi.co/api/v2/pokemon/{id}", null);
+                string content = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrWhiteSpace(content))
+                    throw new Exception("The server returned no results!");
+                if (content.ToLower().Contains("not found"))
+                    throw new Exception($"Pokémon with ID #{id} not found!");
+                return JsonConvert.DeserializeObject<Pokemon>(content);
+            }
+        }
+    }
+}
